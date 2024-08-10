17c67070e214d835306baac67e1e2f07e8955701
Evgenii Gorokhov <enigorohov@yandex.kz>
2024-08-10 17:26:14 +0500

    add the project name to README

--- tests.py
+++ tests.py
@@ -2,23 +2,137 @@ from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
+import pytest
+
+from main import BooksCollector
+
+
+# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
+# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
+
     def test_add_new_book_add_two_books(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize('name', [
+        'Гордость и предубеждение и зомби'
+        'Что делать, если ваш кот хочет вас убить'
+    ])
+    def test_add_new_book_with_a_long_title(self, name):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+        assert len(name) > 41, f"Ошибка: Длина названия книги '{name}' должна быть больше 41 символа."
+
+
+    @pytest.mark.parametrize('name, genre', [
+        ("Властелин Колец", "Фантастика"),
+        ("Шерлок Холмс", "Детективы")
+    ])
+    def test_set_book_genre(self, name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        actual_genre = collector.get_book_genre(name)
+        assert actual_genre == genre
+
+    def test_set_book_genre_invalid(self):
+        collector = BooksCollector()
+        collector.add_new_book('1984')
+        collector.set_book_genre('1984', 'Invalid Genre')
+        assert collector.get_book_genre('1984') == ""
+
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Автостопом по Галактике')
+        collector.set_book_genre('Автостопом по Галактике', 'Фантастика')
+        collector.add_new_book('Dracula')
+        collector.set_book_genre('Dracula', 'Ужасы')
+        books = collector.get_books_with_specific_genre('Фантастика')
+        assert 'Автостопом по Галактике' in books
+        assert 'Dracula' not in books
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book('Лев, колдунья и платяной шкаф')
+        collector.set_book_genre('Лев, колдунья и платяной шкаф', 'Фантастика')
+        collector.add_new_book('Тебе не спрятаться')
+        collector.set_book_genre('Тебе не спрятаться', 'Ужасы')
+        books = collector.get_books_for_children()
+        assert 'Лев, колдунья и платяной шкаф' in books
+        assert 'Тебе не спрятаться' not in books
+
+        # добавляем книгу в Избранное
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_book_in_favorites('Дюна')
+        assert 'Дюна' in collector.get_list_of_favorites_books()
+
+        # удаляем книгу из Избранного
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Дюна')
+        collector.add_book_in_favorites('Дюна')
+        collector.delete_book_from_favorites('Дюна')
+        assert 'Дюна' not in collector.get_list_of_favorites_books()
+
+        # добавить книгу в избранное несуществующую
+
+    def test_add_book_in_favorites_invalid(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Nonexistent Book')
+        assert 'Nonexistent Book' not in collector.get_list_of_favorites_books()
+
+        # получаем список Избранных книг
+
+    def test_get_list_in_favorites_books(self):
+        collector = BooksCollector()
+        assert collector.get_list_of_favorites_books() == []
+
+    def test_get_list_in_favorites_books_single(self):
+        collector = BooksCollector()
+        collector.add_new_book('The Great Gatsby')
+        collector.add_book_in_favorites('The Great Gatsby')
+        assert collector.get_list_of_favorites_books() == ['The Great Gatsby']
+
+    def test_get_list_in_favorites_books_multiple(self):
+        collector = BooksCollector()
+        collector.add_new_book('1984')
+        collector.add_new_book('Brave New World')
+        collector.add_book_in_favorites('1984')
+        collector.add_book_in_favorites('Brave New World')
+        assert collector.get_list_of_favorites_books() == ['1984', 'Brave New World']
+
+
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+        assert collector.books_genre == {}
+
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        assert collector.get_book_genre('Гордость и предубеждение') == ''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
